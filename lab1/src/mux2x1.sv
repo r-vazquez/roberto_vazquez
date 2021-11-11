26f90a564f4c14b08c2f0052cbec9b86b9421a85
Roberto Vázquez <roberto.vazquezv@iteso.mx>
2021-11-10 19:58:32 -0700

    Actualizacion de estilo de codificacion

--- lab1/src/mux2x1.sv
+++ lab1/src/mux2x1.sv
@@ -1,11 +1,15 @@
-module mux2x1 (d0, d1, sel, mux_out);
+// Coder:           Roberto Vazquez Vega
+// Date:            10 November 2021
+// Name:            mux2x1.sv
+// Description:     2 to 1 multiplexer. 
 
-	parameter DW = 		2; //DATA WIDTH
-
-	input 			sel; // SELECTOR
-	input logic [DW-1:0] 	d0; //FIRST INPUT
-	input logic [DW-1:0] 	d1; //SECOND INPUT
-	output logic [DW-1:0] 	mux_out; //MUX OUTPUT
+module mux2x1 #(
+	parameter 		DW = 2 	  //DATA WIDTH
+	)( 
+	input 			sel, 	  // SELECTOR
+	input logic [DW-1:0] 	d0, 	  //FIRST INPUT
+	input logic [DW-1:0] 	d1, 	  //SECOND INPUT
+	output logic [DW-1:0] 	mux_out); //MUX OUTPUT
 
 	assign mux_out = (sel) ? d1 : d0;
 
--- lab1/tb/TB_mux2x1.sv
+++ lab1/tb/TB_mux2x1.sv
@@ -1,34 +1,31 @@
-`timescale 1ns / 100ps
-module TB_mux2x1 (d0, d1, sel, mux_out);
+// Coder:           Roberto Vazquez Vega
+// Date:            10 November 2021
+// Name:            TB_mux2x1.sv
+// Description:     Testbench for 2 to 1 multiplexer. 
 
-        parameter DW = 		4; //DATA WIDTH
+`timescale 1ns / 1ps
+module TB_mux2x1 ();
+        parameter 	DW = 4;  //DATA WIDTH
 
-        input logic 			sel; // SELECTOR
-        input logic [DW-1:0] 		d0; //FIRST INPUT
-        input logic [DW-1:0] 		d1; //SECOND INPUT
-        output logic [DW-1:0] 		mux_out; //MUX OUTPUT
+        logic 		sel;     // SELECTOR
+        logic [DW-1:0] 	d0;      //FIRST INPUT
+        logic [DW-1:0] 	d1;      //SECOND INPUT
+        logic [DW-1:0] 	mux_out; //MUX OUTPUT 
 
-        
+	//MUX INSTANTATION
 	mux2x1 #(.DW(DW)) UUT (.d0(d0), .d1(d1), .sel(sel), .mux_out(mux_out));
 
 	initial begin
-	
-		#10
-		d0 = 'b0110;
+	#10	d0 = 'b0110; //INITIALIZING INPUTS
 		d1 = 'b1001;
-		sel = 'b0;
+		sel = 'b0;   //0
 
-		#10
-		sel = 'b1;
-		
-		#10
-		sel = 'x;
+	#10	sel = 'b1;   //1
 	
-		#10
-		sel = 'z;
+	#10	sel = 'x;    //LOGIC VALUE IS UNKNOWN
+	
+	#10	sel = 'z;    //HIGH IMPEDANCE STATE
 
-		#10;
-	
+	#10;
 	end
-
 endmodule
